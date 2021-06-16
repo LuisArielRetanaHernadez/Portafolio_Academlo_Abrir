fde0d97469289bd5d62f1f2bb7451840652edc49
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2021-06-16 18:50:55 -0500

    efectos_scroll

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,34 +1,23 @@
-/* let pala = ['2','eaf','vsi','vdfv']
-let i = 0
-let timer_is_on = 0
-function timedCount(){
-    //console.log(pala[i])
-    i++
-    if(i===pala.length){
-        i=0
-    }
-    setTimeout(timedCount,2000)
-}
-timedCount() */
 
-//  window.sr = ScrollReveal();
 
-// sr.reveal('.project',{
-//     duration: 4000,
-//     origin: 'top',
-//     distance: '-50px'
-// })
+ window.sr = ScrollReveal();
 
-// sr.reveal('.about-feactures',{
-//     duration: 2000,
-//     origin: 'right',
-//     distance: '-100px'
-// })   
-// sr.reveal('.about-story',{
-//     duration: 2000,
-//     origin: 'left',
-//     distance: '-50px'
-// }) 
+sr.reveal('.project',{
+    duration: 4000,
+    origin: 'top',
+    distance: '-10px'
+})
+
+sr.reveal('.about-feactures',{
+    duration: 2000,
+    origin: 'right',
+    distance: '-10px'
+})   
+sr.reveal('.about-story',{
+    duration: 2000,
+    origin: 'left',
+    distance: '-10px'
+}) 
 
 window.addEventListener('scroll', function(){
     const nav = document.querySelector('.container-menu')
